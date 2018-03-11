fe92448c09ea4c4b9c7b148ed029359ada1b697b
root <root@Omega-6DC9.lan>
2018-03-11 18:20:37 +0000

    Frame parser & float handling

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ var SerialPort = serialport.SerialPort;
 var NewFrameReceived = false;
 var waitingForFrame = false;
 //var seriaRxBuffer = new Buffer();
+var getData_queue = 0;
+
+var MagX = 0;
+var MagY = 0;
+var MagZ = 0;
+
+var GyroX = 0;
+var GyroY = 0;
+var GyroZ = 0;
+
+var AccelX = 0;
+var AccelY = 0;
+var AccelZ = 0;
 
 //------------------- Obs³uga portu szeregowego --------------------------------
 var port = new SerialPort('/dev/ttyS1', {
@@ -28,6 +41,17 @@ port.on('open', function () {
 port.on('data', function (data) {
 	NewFrameReceived = true;
 	console.log('Data: ' + data.toString('hex'));
+	switch(data[3]){
+		case 1: console.log('Mag data'); break;
+		case 2: console.log('Gyro data'); break;
+		case 3: console.log('Accel data'); break;
+		case 4: console.log('State data'); break;
+		default: console.log('Wrong data'); break;
+	}
+	decodeFrame(data);
+	console.log('MagX: ' + MagX + '\t\tGyroX: ' + GyroX + '\tAccelX: ' + AccelX);
+	console.log('MagY: ' + MagY + '\t\tGyroY: ' + GyroY + '\tAccelY: ' + AccelY);
+	console.log('MagZ: ' + MagZ + '\t\tGyroZ: ' + GyroZ + '\tAccelZ: ' + AccelZ);
 });
 
 
@@ -42,12 +66,64 @@ var requestState = [0xAA, 0xAA, 0x02, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
 setInterval(sensorUpdate, 1000);
 
 function sensorUpdate() {
-		getMagData();
+	switch(getData_queue){
+		case 0:  getMagData(); 		break;
+		case 1:  getGyroData(); 	break;
+		case 2:  getAccelData(); 	break;
+		case 3:  getStateData(); 	break;
+		default: getData_queue = 0; 	break;
+	}
+	if(getData_queue < 3) getData_queue++;
+	else getData_queue = 0;
 }
 
 function getMagData() {
 	port.write(requestMag);
 	waitingForFrame = true;
 	port.flush();	//czyœci bufor RX ze wszystkich danych
-	//seriaRxBuffer = new Buffer();	//czyszczenie bufora
+}
+
+function getGyroData() {
+	port.write(requestGyro);
+	waitingForFrame = true;
+	port.flush();	//czyœci bufor RX ze wszystkich danych
+}
+
+function getAccelData() {
+	port.write(requestAccel);
+	waitingForFrame = true;
+	port.flush();	//czyœci bufor RX ze wszystkich danych
+}
+
+function getStateData() {
+	port.write(requestState);
+	waitingForFrame = true;
+	port.flush();	//czyœci bufor RX ze wszystkich danych
+}
+
+function decodeFrame(data){
+	var view = new DataView(new ArrayBuffer(20));
+	
+	// set bytes
+	data.forEach(function (b, i) {
+    		view.setUint8(i, b);
+	});
+
+	if(data[3] == 1){
+		MagX = view.getFloat32(4, true);
+		MagY = view.getFloat32(8, true);
+		MagZ = view.getFloat32(12, true);
+	}
+	else if(data[3] == 2){
+		GyroX = view.getFloat32(4, true);
+		GyroY = view.getFloat32(8, true);
+		GyroZ = view.getFloat32(12, true);
+	}
+	else if(data[3] == 3){
+		AccelX = view.getFloat32(4, true);
+		AccelY = view.getFloat32(8, true);
+		AccelZ = view.getFloat32(12, true);
+	}
+
+	console.log('Frame decoded!');
 }
